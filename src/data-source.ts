431a7949a5492b538fc9cfbbbe1f5b662aa6a32c
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-02 15:58:59 +0300

    db production error alt conn try 7 removed port

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST_PRODUCTION,
-    port: Number(process.env.DB_PORT),
+    // port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
