fbdafdedb6064d264dd56b6fe99c0b85f9e1623e
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-02 14:14:26 +0300

    Production server initlized

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_PRODUCTION,
     port: 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,8 @@
 declare namespace NodeJS {
     interface ProcessEnv {
       PORT: string;
-      DB_HOST: string;
+      DB_HOST_DEV: string;
+      DB_HOST_PRODUCTION: string;
       DB_USER: string;
       DB_PASSWORD: string;
       DB_NAME: string;
