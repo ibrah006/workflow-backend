7a8d8c1373e6ec78c0f48c209ff45e4757b7c183
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-02 15:31:27 +0300

    db production error alt conn try 4

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    url: process.env.DB_URL,
+    // url: process.env.DB_URL,
     entities: ["src/models/*.ts"],
     migrations: ["src/migration/**/*.ts"],
     ssl: {
