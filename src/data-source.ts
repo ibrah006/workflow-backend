06359af7d32abd9bd5018ffd4f142c748a3bed2a
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-02 14:46:40 +0300

    db production url setup

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,6 +12,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    url: process.env.DB_URL,
     synchronize: true,
     entities: ["src/models/*.ts"],
     migrations: ["src/migration/**/*.ts"],
