d4a0f58434ae34b44c97a97e0686b40aca692c8d
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-02 16:13:33 +0300

    db production error alt conn try 9

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,29 +5,29 @@ dotenv.config();
 
 // console.log("database name:", process.env.DB_NAME);
 
+// export const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: process.env.DB_HOST_PRODUCTION,
+//     port: Number(process.env.DB_PORT),
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     url: process.env.DB_URL,
+//     entities: ["src/models/*.ts"],
+//     migrations: ["src/migration/**/*.ts"],
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST_PRODUCTION,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    url: process.env.DB_URL,
+    url: process.env.DB_URL, // Render connection string
+    synchronize: false, // Set to true only for dev
+    logging: false,
     entities: ["src/models/*.ts"],
     migrations: ["src/migration/**/*.ts"],
     ssl: {
-        rejectUnauthorized: true
-    }
-});
-
-// export const AppDataSource = new DataSource({
-//     type: 'postgres',
-//     url: process.env.DB_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     },
-//     synchronize: true,
-//     entities: ["src/models/*.ts"],
-//     migrations: ["src/migration/**/*.ts"],
-//   });
-
+      rejectUnauthorized: false, // Required for Render
+    },
+  });
