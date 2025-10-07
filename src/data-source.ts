924b55fe2246f82a34f87db156ce3226c7e9faff
ibrahimmn <ibrahimmn006@gmail.com>
2025-10-07 15:06:42 +0300

    minor problem fixed

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,29 +5,29 @@ dotenv.config();
 
 // console.log("database name:", process.env.DB_NAME);
 
-// export const AppDataSource = new DataSource({
-//     type: 'postgres',
-//     host: process.env.DB_HOST_PRODUCTION,
-//     port: Number(process.env.DB_PORT),
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     url: process.env.DB_URL,
-//     entities: ["dist/models/*.js"],
-//     migrations: ["dist/migrations/*.js"],
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    url: process.env.DB_URL, // Render connection string
-    synchronize: false, // Set to true only for dev
-    logging: false,
-    entities: ["src/models/*.ts"],
-    migrations: ["src/migration/*.ts"],
+    host: process.env.DB_HOST_PRODUCTION,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    url: process.env.DB_URL,
+    entities: ["dist/models/*.js"],
+    migrations: ["dist/migrations/*.js"],
     ssl: {
-      rejectUnauthorized: false, // Required for Render
-    },
-  });
+        rejectUnauthorized: false
+    }
+});
+
+// export const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     url: process.env.DB_URL, // Render connection string
+//     synchronize: false, // Set to true only for dev
+//     logging: false,
+//     entities: ["src/models/*.ts"],
+//     migrations: ["src/migration/*.ts"],
+//     ssl: {
+//       rejectUnauthorized: false, // Required for Render
+//     },
+//   });
