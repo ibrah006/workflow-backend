a73cfad3a4bf5f5372e973e7425eee45a00ff966
ibrahimmn <ibrahimmn006@gmail.com>
2025-12-11 10:51:51 +0300

    task attribute department and migration

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -78,6 +78,9 @@ export class Task {
     @Column({ type: 'timestamptz', nullable: true })
     productionStartTime?: Date | null;
 
+    @Column()
+    department!: string;
+
     // All the Material logs initiated for this task
     // @OneToMany(()=> MaterialLog, (log)=> log.task)
     // materialLogs!: MaterialLog[];
