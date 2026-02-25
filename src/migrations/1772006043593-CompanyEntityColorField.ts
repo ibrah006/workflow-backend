aa4d655131f1608b935c2e2e1fe5ef1f9aec2db4
ibrahimmn <ibrahimmn006@gmail.com>
2026-02-25 11:54:38 +0400

    company entity color field migration

--- src/migrations/1772006043593-CompanyEntityColorField.ts
+++ src/migrations/1772006043593-CompanyEntityColorField.ts
@@ -1,16 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Updates1772005927448 implements MigrationInterface {
-    name = 'Updates1772005927448'
+export class CompanyEntityColorField1772006043593 implements MigrationInterface {
+    name = 'CompanyEntityColorField1772006043593'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "company" ADD "email" character varying`);
         await queryRunner.query(`ALTER TABLE "company" ADD "industry" character varying`);
         await queryRunner.query(`ALTER TABLE "company" ADD "phone" character varying`);
         await queryRunner.query(`ALTER TABLE "company" ADD "contactName" character varying`);
+        await queryRunner.query(`ALTER TABLE "company" ADD "color" character varying(7)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "color"`);
         await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "contactName"`);
         await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "phone"`);
         await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "industry"`);
